7446b69c6e6ad222beedba0988c8f2af69a72173
Annette Maza <nettymaza@MacBook-Pro.local>
2017-12-29 15:28:33 -0700

    Created a sessions controller and login page, and helper methods.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(email: params[:session][:email])
+
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            redirect_to user
+        else
+            redirect_to controller: 'sessions', action: 'new'
+        end
     end
 
     def destroy
+        session.delete :user_id
+        redirect_to '/login'
     end
 end
