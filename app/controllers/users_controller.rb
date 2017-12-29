1aa9c246b26e440db7bbc4a107d86e79c2d72fe6
Annette Maza <nettymaza@MacBook-Pro.local>
2017-12-29 15:27:31 -0700

    Added User model controller, signup form and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,28 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show]
+    def show
+    end
     def new
         @user = User.new
     end
 
-    # def create
-    #     @user = User.create(user_params)
-    # end
-    #
-    # private
-    #
-    #     def user_params
-    #         params.require(user).permit(:name, :password, :password_confirmation)
-    #     end
+    def create
+        @user = User.create(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+        def set_user
+            @user = User.find(params[:id])
+        end
+
+        def user_params
+            params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
 end
