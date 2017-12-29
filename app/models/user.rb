f60ab6d2f2e5198d61016638c9c427a379edc9ee
Annette Maza <nettymaza@MacBook-Pro.local>
2017-12-29 15:29:40 -0700

    Added Active record validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-     validates :name, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
+    validates :name, presence: true
+    validates :email, uniqueness: true
 
 end
