b8a798759bdc2324a21a116087968242cd1703b6
Annette Maza <nettymaza@MacBook-Pro.local>
2018-01-03 14:47:29 -0700

    added error messages partial to clean up my user views. Included partial in sign up form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
     validates :name, presence: true
-    validates :email, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
 
 end
